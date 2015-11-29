50988a7c1aac202cfa0a4984a8ba87e178ba0bd8
HawesDomingue <HawesDomingue@utexas.edu>
2015-11-29 17:17:47 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+/**
+ * Created by PhpStorm.
+ * User: Wedge
+ * Date: 11/29/2015
+ * Time: 4:10 PM
+ */
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
