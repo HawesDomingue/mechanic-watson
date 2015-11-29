af1c0931f7dbda90f5c071c09ff96d34713f582d
HawesDomingue <HawesDomingue@utexas.edu>
2015-11-29 17:39:14 -0600

    Removing comments

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-/**
- * Created by PhpStorm.
- * User: Wedge
- * Date: 11/29/2015
- * Time: 4:10 PM
- */
-
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Mechanic Watson</title>
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?>
