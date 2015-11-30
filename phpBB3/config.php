d2051bef52a102d1632dee72f042c41e64635a01
HawesDomingue <HawesDomingue@utexas.edu>
2015-11-29 20:05:22 -0600

    Fixing config.php for phpBB

---
+++ phpBB3/config.php
@@ -0,0 +1,18 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'postgres';
+$dbhost = 'ec2-54-83-204-228.compute-1.amazonaws.com';
+$dbport = '5432';
+$dbname = 'd6jnf4k3018dk1';
+$dbuser = 'bwjzkzoepaueey';
+$dbpasswd = '-bz98u8LgOumglTbOXO6qlBMyu';
+
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+//@define('DEBUG', true);
+//@define('DEBUG_EXTRA', true);
+?>
