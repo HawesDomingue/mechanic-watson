c82b00d40d4822e7ac3d1b3b2b9c8d0dc6ebfceb
HawesDomingue <HawesDomingue@utexas.edu>
2015-11-30 00:05:39 -0600

    Testing

--- forum/cache/cache_quickjump_3.php
+++ forum/cache/cache_quickjump_3.php
@@ -7,8 +7,11 @@ $forum_id = isset($forum_id) ? $forum_id : 0;
 ?>				<form id="qjump" method="get" action="viewforum.php">
 					<div><label><span><?php echo $lang_common['Jump to'] ?><br /></span>
 					<select name="id" onchange="window.location=('viewforum.php?id='+this.options[this.selectedIndex].value)">
-						<optgroup label="Test category">
-							<option value="1"<?php echo ($forum_id == 1) ? ' selected="selected"' : '' ?>>Test forum</option>
+						<optgroup label="Automotive Troubleshooting">
+							<option value="2"<?php echo ($forum_id == 2) ? ' selected="selected"' : '' ?>>Unanswered Questions</option>
+						</optgroup>
+						<optgroup label="Android App">
+							<option value="3"<?php echo ($forum_id == 3) ? ' selected="selected"' : '' ?>>App Feedback</option>
 						</optgroup>
 					</select></label>
 					<input type="submit" value="<?php echo $lang_common['Go'] ?>" accesskey="g" />
